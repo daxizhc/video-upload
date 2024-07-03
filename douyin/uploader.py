2c12b2f7a11d14aa00c44b176d0918fbb217e3f2
haochen.zhang <empty@gmail.com>
2024-07-03 16:01:24 +0800

    log

--- douyin/uploader.py
+++ douyin/uploader.py
@@ -1,7 +1,12 @@
 import asyncio
+import sys
 from pathlib import Path
-
+import logging
 from playwright.async_api import async_playwright
+
+logger = logging.getLogger("douyin-uploader")
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
 async def fetch_cookies():
@@ -16,15 +21,21 @@ async def fetch_cookies():
 
         page = await context.new_page()
 
+        logger.info("开始打开登录页面")
         await page.goto(url="https://creator.douyin.com/", timeout=20000)
+        logger.info("打开登录页面成功")
 
         while 'https://creator.douyin.com/creator-micro/home' not in page.url:
+            logger.info("等待登录...")
             await asyncio.sleep(5)
 
+        logger.info("登录成功")
         cookies_path = Path(Path(__file__).parent.resolve() / "cookies.json")
         # 保存cookie
         await context.storage_state(path=cookies_path)
+        logger.info("保存cookie成功")
 
         await context.close()
         await browser.close()
         await playwright.stop()
+        logger.info("退出浏览器")
